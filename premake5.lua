039b77e97c0fef2688c60d10ba7ccccb681e9661
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-12 21:53:02 -0600

    update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-	targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
-	objdir ("bin-int/" .. OutputDir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
